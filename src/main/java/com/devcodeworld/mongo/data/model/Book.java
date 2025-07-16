4e6e760e18e99cca30b0570c9ada3b6141ecf87f
Pattabhi A <pattabhi.amperayani@gmail.com>
2025-07-16 17:04:11 +0530

    Configuring cloud cluster creds

--- src/main/java/com/devcodeworld/mongo/data/model/Book.java
+++ src/main/java/com/devcodeworld/mongo/data/model/Book.java
@@ -1,14 +1,13 @@
 package com.devcodeworld.mongo.data.model;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @ToString
-@Document(collection = "books")
+@Document(collection = "bookstore")
 public class Book {
 
     @Id
